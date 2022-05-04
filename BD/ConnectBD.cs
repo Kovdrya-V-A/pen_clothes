a6176f8425fd955a593509e32b762645ba2dcd52
Kovdrya-V-A <KovdryaVladislav@gmail.com>
2022-05-04 19:04:35 +0300

    workWithListView

---
+++ BD/ConnectBD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pen_clothes.BD
+{
+    public class ConnectBD
+    {
+        public static pen_clothesEntities DataBase = new pen_clothesEntities();
+    }
+}
